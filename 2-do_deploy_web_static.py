f267aacdeacba4ecea9b03779d4a31913ffa821f
Nababa Abdulrahmon <68295577+nababa3@users.noreply.github.com>
2023-10-05 22:45:50 +0100

    web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,48 @@
 #!/usr/bin/python3
-"""A fabric script that distributes an archive to your web servers, using the
-function do_deploy
-"""
-
-from fabric.api import *
+# Fabfile to distribute an archive to a web server.
 import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-
-env.hosts = ['101.188.67.134', '101.188.67.134']
+env.hosts = ["54.165.75.99", "54.209.246.163"]
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers"""
-    if not os.path.exists(archive_path):
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if os.path.isfile(archive_path) is False:
         return False
-    archive_list = archive_path.split("/")
-    archive_filename = archive_list[-1]
-    filename_list = archive_filename.split(".")
-    filename_noext = filename_list[0]
-    result = put(archive_path, "/tmp/")
-    if result.failed:
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    result = run('mkdir -p /data/web_static/releases/{:}/'.format(
-        filename_noext))
-    if result.failed:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    result = run('tar -xzf /tmp/{:} -C /data/web_static/releases/{:}/'.format(
-        archive_filename, filename_noext))
-    if result.failed:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
-    result = run('rm /tmp/{:}'.format(archive_filename))
-    if result.failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
         return False
-    result = run('mv /data/web_static/releases/{:}/web_static/* \
-                 /data/web_static/releases/{:}/'.format(
-                     filename_noext, filename_noext))
-    if result.failed:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    result = run("rm -rf /data/web_static/releases/{:}/web_static".format(
-        filename_noext))
-    if result.failed:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    result = run('rm -rf /data/web_static/current')
-    if result.failed:
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
         return False
-    result = run(
-        'ln -s /data/web_static/releases/{:}/ /data/web_static/current'.format(
-            filename_noext))
-    if result.failed:
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
         return False
     return True
